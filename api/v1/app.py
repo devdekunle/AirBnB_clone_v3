c2029cf500d177fbb3937a6d2adbb2b5e6ca48db
Kaegan Davis Haidome <obichi@coseo.com>
2023-05-01 23:55:25 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
 """
-creating an api
+This module contains a simple flask application
 """
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
 from os import getenv
 
-db_host = getenv('HBNB_API_HOST')
-db_port = getenv('HBNB_API_PORT')
 
+host = getenv("HBNB_API_HOST")
+port = getenv("HBNB_API_PORT")
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={'/*': {'origins': app_host}})
-
-
-@app.teardown_appcontext
-def tear_down(exc):
-    """ close a data base session """
-    storage.close()
+cors = CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """ method for when page requested is not found"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def error_handler(error):
+    """handle 404 errors"""
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """handle exit"""
+    storage.close()
 
 
 if __name__ == "__main__":
-    host = db_host or "0.0.0.0"
-    port = db_port or 5000
+    if not host:
+        host = "0.0.0.0"
+    if not port:
+        port = 5000
     app.run(host=host, port=port, threaded=True, debug=True)
