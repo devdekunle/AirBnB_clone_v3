fab5ef6e317b0be36e621e4987acc31f6af71893
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-05-01 13:29:24 +0100

    fix pep8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,10 +7,12 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects"""
     return jsonify([state.to_dict() for state in storage.all(State).values()])
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -22,6 +24,7 @@ def get_state(state_id):
         return jsonify(state.to_dict())
     else:
         return None
+
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
@@ -35,6 +38,7 @@ def delete_state(state_id):
         storage.save()
         return make_response(jsonify({}), 200)
     return None
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -62,6 +66,7 @@ def create_state():
 
     # Return a JSON response with the new State object and a 201 status code
     return make_response(jsonify(state.to_dict()), 201)
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
